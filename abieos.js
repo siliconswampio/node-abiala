f1d9cd963644d1410a8e6e1bbe67f603a3033b48
Igor Lins e Silva <igor.linsesilva@gmail.com>
2020-02-13 01:38:05 -0300

    fix import path

--- abieos.js
+++ abieos.js
@@ -1,5 +1,5 @@
 if (process.platform === "linux") {
-    const abieos = require('./build/Release/abieos.node');
+    const abieos = require('./dist/abieos.node');
     module.exports = abieos;
 } else {
     module.exports = null;
